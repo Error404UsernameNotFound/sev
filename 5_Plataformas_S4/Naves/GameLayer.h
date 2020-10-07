90069799df0fa4e43983eeb757b2e23ece901d5e
Raúl M <60326859+Error404UsernameNotFound@users.noreply.github.com>
2020-10-07 11:15:27 +0200

    Initial commit for sesion 4 practice

---
+++ 5_Plataformas_S4/Naves/GameLayer.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "Layer.h"
+#include "Player.h"
+#include "Background.h"
+#include "Enemy.h"
+#include "Projectile.h"
+#include "Tile.h"
+
+#include "Text.h"
+
+#include "Audio.h"
+#include "Space.h" 
+
+#include <sstream> //Leer lineas (String)
+#include <fstream> //Leer ficheros
+#include <list>
+
+
+class GameLayer : public Layer {
+public:
+	GameLayer(Game* game);
+	void init() override;
+	void processControls() override;
+	void update() override;
+	void draw() override;
+
+	void keysToControls(SDL_Event event);
+	int newEnemyTime = 0;
+
+	Player* player;
+	Background* background;
+	Actor* backgroundPoints;
+
+	bool controlShoot = false;
+	int controlMoveY = 0;
+	int controlMoveX = 0;
+
+	list<Enemy*> enemies;
+	list<Projectile*> projectiles;
+
+	Audio* audioBackground;
+
+	Text* textPoints;
+	int points; //Revisar
+
+	list<Tile*> tiles;
+	int mapWidth;
+	void loadMap(string name);
+	void loadMapObject(char character, float x, float y);
+	void calculateScroll();
+	Space* space;
+	float scrollx;
+};
+
