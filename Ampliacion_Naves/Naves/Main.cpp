875c98b90f744add0bdb1069270b8f7ed299d4d4
Raúl M <60326859+Error404UsernameNotFound@users.noreply.github.com>
2020-11-02 02:42:23 +0100

    Added new project for "Naves" ampliation

---
+++ Ampliacion_Naves/Naves/Main.cpp
@@ -0,0 +1,8 @@
+#include "Game.h"
+#undef main //Una libreria ya tiene main definido, evita choques
+
+int main() {
+	Game* game = new Game();
+
+	return 0;
+}
---
+++ Ampliacion_Naves/Naves/Player.cpp
@@ -0,0 +1,38 @@
+#include "Player.h"
+
+Player::Player(float x, float y, Game* game) :
+	Actor("res/jugador.png", x, y, 50, 57, game) {
+	audioShoot = new Audio("res/efecto_disparo.wav", false);
+
+	//TODO revisar 
+	//fileWidth = 50;
+}
+
+void Player::update() {
+	if (shootTime > 0) {
+		shootTime--;
+	}
+
+	x = x + vx;
+	y = y + vy;
+}
+
+void Player::moveX(float axis) {
+	vx = axis * 3;
+}
+
+void Player::moveY(float axis) {
+	vy = axis * 3;
+}
+
+Projectile* Player::shoot() {
+	if (shootTime == 0) {
+		audioShoot->play();
+
+		shootTime = shootCadence;
+		return new Projectile(x, y, game);
+	}
+	else {
+		return NULL;
+	}
+}
